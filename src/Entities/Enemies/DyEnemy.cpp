5cddbc48e01ccf22efbcd184ec386ba1410f38eb
Gomzz-otb <annier.gomez@upr.edu>
2026-03-14 21:26:18 -0400

    Add phase 3 enemy sounds and DyEnemy texture variants

--- src/Entities/Enemies/DyEnemy.cpp
+++ src/Entities/Enemies/DyEnemy.cpp
@@ -3,9 +3,9 @@
 
 void DyEnemy::draw() {
     if (HitBox::drawHitbox) this->hitBox.draw();
-    DrawTexturePro(ImageManager::SpriteSheet, this->spriteRec, 
-            Rectangle{this->position.first + 15, this->position.second + 15, 30, 30}, 
-            Vector2{15, 15}, this->aimAngle, WHITE);
+    DrawTexturePro(ImageManager::SpriteSheet, this->spriteRec,
+    Rectangle{this->position.first + 15, this->position.second + 15, 30, 30},
+    Vector2{15, 15}, this->aimAngle, WHITE);
 }
 
 void DyEnemy::update(std::pair<float, float> pos, HitBox target) {
