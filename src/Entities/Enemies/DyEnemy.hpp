e5cb03e990ab0cec0465de24f17a626007a76cfb
Gomzz-otb <annier.gomez@upr.edu>
2026-03-14 20:17:18 -0400

    Complete phase 3 changes for sounds and DyEnemy textures

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -6,12 +6,19 @@ class DyEnemy : public Enemy {
         float angle = 135;
         float aimAngle = 225;
         bool loop = false;
+        Rectangle spriteRec = Rectangle{2, 128, 13, 14};
 
     public:
-        DyEnemy(float x, float y) : Enemy(x, y) { 
-            this->cooldown = GetRandomValue(90, 300);
-            this->health = 1; 
-        }
+       DyEnemy(float x, float y) : Enemy(x, y) { 
+    this->cooldown = GetRandomValue(90, 300);
+    this->health = 1;
+
+    if (GetRandomValue(0, 1) == 0) {
+        this->spriteRec = Rectangle{2, 128, 13, 14};
+    } else {
+        this->spriteRec = Rectangle{2, 147, 13, 13};
+    }
+}
 
         void draw() override;
         void update(std::pair<float, float> pos, HitBox target) override;
--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -51,18 +51,24 @@ class Enemy {
                     p.second->update(p.first, target);
 
                     for (Projectile& p2 : Projectile::projectiles) {
-                        if (p2.ID != 1 && HitBox::Collision(p.second->hitBox, p2.getHitBox())) {
-                            p.second->health--;
-                            p2.del = true;
-                        }
-                    }
+    if (p2.ID != 1 && HitBox::Collision(p.second->hitBox, p2.getHitBox())) {
+        p.second->health--;
+        p2.del = true;
 
-                    if (p.second->health <= 0) {
-                        Animation::animations.push_back(
-                            Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
-                        );
-                        p.second = nullptr;
-                    }
+        if (p.second->health > 0) {
+            PlaySound(SoundManager::hit);
+        }
+    }
+}
+
+if (p.second->health <= 0) {
+    PlaySound(SoundManager::dead);
+
+    Animation::animations.push_back(
+        Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
+    );
+    p.second = nullptr;
+}
                 }
             }
             
