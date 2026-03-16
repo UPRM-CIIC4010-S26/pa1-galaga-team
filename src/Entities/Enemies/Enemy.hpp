2afa012b7da331da4ccf4431ea1a9d014a3fbeab
5p00k-S0ul-H4ck3r5 <maria.gonzalez164@upr.edu>
2026-03-15 23:32:44 -0400

    FIXED SCORE BUGS

--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -70,14 +70,10 @@ class Enemy {
                     Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
                 );
 
-                p.second = nullptr;
+
             }
-                    if (p.second->health <= 0) {
-                        Animation::animations.push_back(
-                            Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
-                        );
-                      
-                    }
+
+                    
 
 
 
